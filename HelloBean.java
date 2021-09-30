67f69fb395c11925e4c85aaf8353602dbecd6bb9
skaeotjd12@naver.com <skaeotjd12@naver.com>
2021-09-30 14:29:05 +0900

    Add All File

---
+++ HelloBean.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class HelloBean {
+	private String name = "ȫ�浿";
+	private int age = 20;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
